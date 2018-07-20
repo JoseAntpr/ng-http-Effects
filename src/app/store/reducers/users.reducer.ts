036a9f11745a3658bc9860668038df74d54b530e
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-07-20 16:15:04 +0200

    ADD: add users reducer

---
+++ src/app/store/reducers/users.reducer.ts
@@ -0,0 +1,45 @@
+
+import * as fromUsers from '../actions';
+import { User } from '../../models/user.model';
+
+export interface UsersState {
+    users: User[];
+    loaded: boolean;
+    loading: boolean;
+    error: any;
+}
+
+const initialState: UsersState = {
+    users: [],
+    loaded: false,
+    loading: false,
+    error: null
+};
+
+
+export function userReducer( state = initialState, action: fromUsers.actions ): UsersState {
+    switch ( action.type ) {
+        case fromUsers.LOAD_USERS:
+            return {
+                ...state,
+                loading: true
+            };
+        case fromUsers.LOAD_USERS_SUCCESS:
+            return {
+                ...state,
+                users: [...action.users],
+                loading: false,
+                loaded: true
+            };
+        case fromUsers.LOAD_USERS_FAILED:
+            return {
+                ...state,
+                loaded: false,
+                loading: false,
+                error: action.payload
+            };
+        default:
+            return state;
+    }
+}
+
