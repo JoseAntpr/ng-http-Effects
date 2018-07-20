9292370f779516b37be190312a6925261b3bf18b
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-07-20 12:34:48 +0200

    ADD: init project and create the basic directory structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// Personalize modules
+import { SharedModule } from './shared/shared.module';
+import { UsersModule } from './users/users.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    UsersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
